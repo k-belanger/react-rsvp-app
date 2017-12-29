ba544aa9f0e9cb11a6eded2dd95b901963ea3494
KimBelanger <kim@kimberlybelanger.com>
2017-12-29 10:13:40 -0800

    initial commit

---
+++ src/guestList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Guest from './guest';
+
+const GuestList = props => 
+    <ul>
+       {props.guests.map((guest, index) =>
+        <Guest 
+            key={index} 
+            name={guest.name} 
+            isConfirmed={guest.isConfirmed}
+            isEditing={guest.isEditing}
+            handleConfirmation={() => props.toggleConfirmationAt(index)}
+            handleToggleEditing={() => props.toggleEditingAt(index)}
+            setName={text => props.setNameAt(text, index)}
+        />
+       )}
+       
+    </ul>
+
+
+GuestList.propTypes = {
+    guests: PropTypes.array.isRequired,
+    toggleConfirmationAt: PropTypes.func.isRequired,
+    toggleEditingAt: PropTypes.func.isRequired,
+    setNameAt: PropTypes.func.isRequired
+}
+
+export default GuestList;
