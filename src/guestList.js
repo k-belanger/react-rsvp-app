50b720f76e19037811db645e2150a5ac8fdcf15a
KimBelanger <kim@kimberlybelanger.com>
2017-12-29 10:52:23 -0800

    adds confirmed guest filter functionality

--- src/guestList.js
+++ src/guestList.js
@@ -4,16 +4,18 @@ import Guest from './guest';
 
 const GuestList = props => 
     <ul>
-       {props.guests.map((guest, index) =>
-        <Guest 
-            key={index} 
-            name={guest.name} 
-            isConfirmed={guest.isConfirmed}
-            isEditing={guest.isEditing}
-            handleConfirmation={() => props.toggleConfirmationAt(index)}
-            handleToggleEditing={() => props.toggleEditingAt(index)}
-            setName={text => props.setNameAt(text, index)}
-        />
+       {props.guests
+            .filter(guest => !props.isFiltered || guest.isConfirmed)
+            .map((guest, index) =>
+            <Guest 
+                key={index} 
+                name={guest.name} 
+                isConfirmed={guest.isConfirmed}
+                isEditing={guest.isEditing}
+                handleConfirmation={() => props.toggleConfirmationAt(index)}
+                handleToggleEditing={() => props.toggleEditingAt(index)}
+                setName={text => props.setNameAt(text, index)}
+            />
        )}
        
     </ul>
@@ -23,7 +25,8 @@ GuestList.propTypes = {
     guests: PropTypes.array.isRequired,
     toggleConfirmationAt: PropTypes.func.isRequired,
     toggleEditingAt: PropTypes.func.isRequired,
-    setNameAt: PropTypes.func.isRequired
+    setNameAt: PropTypes.func.isRequired,
+    isFiltered: PropTypes.bool.isRequired
 }
 
 export default GuestList;
